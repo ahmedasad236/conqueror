b0f31608b34b7226adf7472a7e4381c4b154f438
ahmedasad236 <ahmedasad236@gmail.com>
2021-08-09 09:39:43 +0200

    Add the abstract class Economic building

---
+++ The_Conqueror/src/buildings/EconomicBuilding.java
@@ -0,0 +1,10 @@
+package buildings;
+
+public abstract class EconomicBuilding extends Building {
+
+    public EconomicBuilding(int cost,int upgradeCost) {
+        super(cost, upgradeCost);
+    }
+    
+}
+
