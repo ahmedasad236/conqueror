9338828386a8dfbd50294db8ca664b6ac86e197f
ahmedasad236 <ahmedasad236@gmail.com>
2021-08-12 00:36:12 +0200

    Add Unit class

---
+++ The_Conqueror/src/units/Unit.java
@@ -0,0 +1,20 @@
+package units;
+
+public abstract class Unit {
+    int level; //The current level of a unit.
+    int maxSoldierCount; //The maximum number of soldiers a unit can hold.
+    int currentSoldierCount; //The current number of soldiers inside a unit.
+    double idleUpkeep; //The amount of food a unit will consume when being idle.
+    double marchingUpkeep; //The amount of food a unit will consume when marching to another city.
+    double siegeUpkeep; //The amount of food a unit will consume when laying siege.
+
+    public Unit(int level,int maxSoldierConunt,double idleUpkeep, double
+            marchingUpkeep,double siegeUpkeep)
+    {
+        this.level = level;
+        this.maxSoldierCount = maxSoldierConunt;
+        this.idleUpkeep = idleUpkeep;
+        this.marchingUpkeep = marchingUpkeep;
+        this.siegeUpkeep = siegeUpkeep;
+    }
+}
