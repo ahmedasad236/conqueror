f153590b3ebfa1a3d02b6dbf2de22eda2a7373c2
ahmedasad236 <68563546+ahmedasad236@users.noreply.github.com>
2021-08-30 01:44:45 +0200

    Add the method Upgrade
    Add  the abstract method in Building class. I overridden it in all subclasses.

--- The_Conqueror/src/buildings/ArcheryRange.java
+++ The_Conqueror/src/buildings/ArcheryRange.java
@@ -1,11 +1,44 @@
 package buildings;
 
+import exceptions.BuildingInCoolDownException;
+import exceptions.MaxLevelException;
+
 public class ArcheryRange extends MilitaryBuilding {
     private final static int ARCH_COST = 1500;
-    private final static int ARCH_UPGRADE_COST = 800;
-    private final static int ARCH_RECRUITMENT_COST = 400;
+    private final static int []ARCH_UPGRADE_COSTS = {800, 700};
+    private final static int []ARCH_RECRUITMENT_COSTS = {400, 450, 500};
+    
 
     public ArcheryRange() {
-        super(ARCH_COST, ARCH_UPGRADE_COST, ARCH_RECRUITMENT_COST);
+        super(ARCH_COST, ARCH_UPGRADE_COSTS[0], ARCH_RECRUITMENT_COSTS[0]);
+    }
+
+    @Override
+    public void upgrade() throws BuildingInCoolDownException, MaxLevelException {
+        
+        int lev = this.getLevel();
+        
+        if(this.isCoolDown())
+            throw new BuildingInCoolDownException();
+        
+
+        switch(lev) {
+            case 1:
+                this.setLevel(2);
+                this.setUpgradeCost(ARCH_UPGRADE_COSTS[1]);
+                this.setCurrentRecuruit(ARCH_RECRUITMENT_COSTS[1]);
+                break;
+            
+            case 2:
+                this.setLevel(3);
+                this.setCurrentRecuruit(ARCH_RECRUITMENT_COSTS[2]);
+                break;
+            
+            default:
+                throw new MaxLevelException();
+        }
+        
+        //this.setCoolDown(false);
+
     }
 }
