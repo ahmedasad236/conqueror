d7db2e92fccc7197d4d4bdf30e8717baa086c03f
Omarkhaled711 <77131348+Omarkhaled711@users.noreply.github.com>
2021-08-30 12:25:04 +0200

    overrode harvest method in the subclasses
    overrode it in farm & market subclasses

--- The_Conqueror/src/buildings/Farm.java
+++ The_Conqueror/src/buildings/Farm.java
@@ -7,12 +7,16 @@ public class Farm extends EconomicBuilding {
     
     private final static int FARM_COST = 1000;
     private final static int []FARM_UPGRADE_COSTS = {500, 700};
-    
+    private final static int[] FARM_HARVEST={500,700,1000};
     public Farm() {
         super(FARM_COST, FARM_UPGRADE_COSTS[0]);
     }
 
-
+    @Override
+    public int harvest() {        
+        return FARM_HARVEST[getLevel()-1];
+    }
+    
     @Override
     public void upgrade() throws BuildingInCoolDownException, MaxLevelException {
             
--- The_Conqueror/src/buildings/Market.java
+++ The_Conqueror/src/buildings/Market.java
@@ -5,9 +5,15 @@ import exceptions.MaxLevelException;
 public class Market extends EconomicBuilding {
     private final static int MARKET_COST = 1500;
     private final static int []MARKET_UPGRADE_COSTS = {700, 1000};
+    private final static int[] MARKET_HARVEST={1000,1500,2000};
 
     public Market() {
         super(MARKET_COST, MARKET_UPGRADE_COSTS[0]);
+    }
+
+    @Override
+    public int harvest() {
+        return MARKET_HARVEST[getLevel()-1];
     }
 
     @Override
