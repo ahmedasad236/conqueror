44ffb04afb7a24fc68dfc57646d867c2c2781f8f
Omarkhaled711 <77131348+Omarkhaled711@users.noreply.github.com>
2021-09-01 00:37:23 +0200

    Added couple of methods and attribuites
    Methods:
    attack
    setters for the attributes
    getter for ParentArmy attribute
    
    attributes:
    ArcherFactor
    CavalryFactor
    InfantryFactor
    ParentArmy

--- The_Conqueror/src/units/Infantry.java
+++ The_Conqueror/src/units/Infantry.java
@@ -5,5 +5,8 @@ public class Infantry extends Unit {
     public Infantry(int level, int maxSoldierConunt, double idleUpkeep, double marchingUpkeep, double siegeUpkeep)
     {
         super(level, maxSoldierConunt, idleUpkeep, marchingUpkeep, siegeUpkeep);
+        setArcherFactor(0.3, 0.4, 0.5);
+        setInfantryFactor(0.1, 0.2, 0.3);
+        setCavalryFactor(0.1, 0.2, 0.25);
     }
 }
--- The_Conqueror/src/units/Unit.java
+++ The_Conqueror/src/units/Unit.java
@@ -1,5 +1,7 @@
 package units;
 
+import exceptions.FriendlyFireException;
+
 public abstract class Unit {
     private int level; //The current level of a unit.
     private int maxSoldierCount; //The maximum number of soldiers a unit can hold.
@@ -7,7 +9,11 @@ public abstract class Unit {
     private double idleUpkeep; //The amount of food a unit will consume when being idle.
     private double marchingUpkeep; //The amount of food a unit will consume when marching to another city.
     private double siegeUpkeep; //The amount of food a unit will consume when laying siege.
-    
+    private double [] ArcherFactor = new double[3]; 
+    private double [] InfantryFactor = new double[3];
+    private double [] CavalryFactor = new double[3];
+    private Army ParentArmy;
+
     public Unit(int level,int maxSoldierConunt,double idleUpkeep, double
             marchingUpkeep,double siegeUpkeep)
     {
@@ -22,7 +28,24 @@ public abstract class Unit {
     public void setCurrentSoldierCount(int count){
         this.currentSoldierCount=count;
     }
-
+    public void setParentArmy(Army Parent){
+        this.ParentArmy=Parent;
+    }
+    protected void setArcherFactor(double x,double y,double z){
+        ArcherFactor[0]=x;
+        ArcherFactor[1]=y;
+        ArcherFactor[2]=z;
+    }
+    protected void setInfantryFactor(double x,double y,double z){
+        InfantryFactor[0]=x;
+        InfantryFactor[1]=y;
+        InfantryFactor[2]=z;
+    }
+    protected void setCavalryFactor(double x,double y,double z){
+        CavalryFactor[0]=x;
+        CavalryFactor[1]=y;
+        CavalryFactor[2]=z;
+    }
     //Getters
     public int getLevel() {
         return level;
@@ -46,6 +69,35 @@ public abstract class Unit {
 
     public double getSiegeUpKeep() {
         return siegeUpkeep;
+    }
+    public Army getParentArmy(){
+        return this.ParentArmy;
+    }
+
+    //other methods
+    public void attack(Unit target)throws FriendlyFireException{
+        if(target.getParentArmy().equals(this.getParentArmy())){
+            throw new FriendlyFireException("Error:You are trying to attack a Friend ");
+        }
+        if(target.getCurrentSoldierCount()<=0){
+            target.setCurrentSoldierCount(0);
+            //handleAttackedUnit(target);
+        }
+        else{
+            if(target instanceof Archer){
+                attack(target, ArcherFactor);
+            }
+            else if(target instanceof Cavalry){
+                attack(target,CavalryFactor);
+            }
+            else{
+                attack(target,InfantryFactor);
+            }
+        }
+
+    }
+    protected void attack(Unit target,double[] attacked){
+        target.setCurrentSoldierCount(target.getCurrentSoldierCount()-(int)(attacked[this.getLevel()-1]*this.getCurrentSoldierCount()));
     }
     
 }
