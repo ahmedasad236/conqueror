853e740c3bc6d5ab3e3b884d80827a843c812e7a
ahmedasad236 <68563546+ahmedasad236@users.noreply.github.com>
2021-09-01 01:32:47 +0200

    Add method getFoodNeeded and Overrode equal method
    The method getFoodNeeded calculates the needed food dependent on the current status and the current number of soldiers.
    I overrode the method equal to be able to find the right unit when I search for it in a list.

--- The_Conqueror/src/units/Unit.java
+++ The_Conqueror/src/units/Unit.java
@@ -7,7 +7,8 @@ public abstract class Unit {
     private double idleUpkeep; //The amount of food a unit will consume when being idle.
     private double marchingUpkeep; //The amount of food a unit will consume when marching to another city.
     private double siegeUpkeep; //The amount of food a unit will consume when laying siege.
-    
+    private Army ParentArmy;
+
     public Unit(int level,int maxSoldierConunt,double idleUpkeep, double
             marchingUpkeep,double siegeUpkeep)
     {
@@ -21,6 +22,10 @@ public abstract class Unit {
     //Setters
     public void setCurrentSoldierCount(int count){
         this.currentSoldierCount=count;
+    }
+
+    public void setParentArmy(Army Parent){
+        this.ParentArmy=Parent;
     }
 
     //Getters
@@ -48,4 +53,40 @@ public abstract class Unit {
         return siegeUpkeep;
     }
     
+    public Army getParentArmy(){
+        return this.ParentArmy;
+    }
+
+    public double getFoodNeeded() {
+        
+        Status stat = ParentArmy.getCurrenStatus();
+        double keepUp = 0.0;
+
+        switch (stat) {
+            case IDLE :
+                keepUp = idleUpkeep;
+                break;
+            
+            case MARCHING:
+                keepUp = marchingUpkeep;
+                break;
+            
+            case BESIEGING:
+                keepUp = siegeUpkeep;
+                break;
+        }    
+
+        return keepUp * currentSoldierCount;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(!(obj instanceof Unit))
+            return false;
+        
+        Unit unit = (Unit)obj;
+        return (this.currentSoldierCount == unit.currentSoldierCount && this.level == unit.level
+        && this.idleUpkeep == unit.idleUpkeep && this.marchingUpkeep == unit.marchingUpkeep &&
+        this.siegeUpkeep == unit.siegeUpkeep && this.ParentArmy == unit.ParentArmy);
+    }    
 }
